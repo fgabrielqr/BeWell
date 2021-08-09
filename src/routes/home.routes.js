f4e1596ea33920df4d9a98ccda6cc02912a279a3
fgabrielqr <fgabrielqr@gmail.com>
2021-08-09 16:56:25 -0300

    feat: :bug: Corrigindo Icones do Tab Navigation

--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Home from '../pages/Home';
 import Conta from '../pages/Conta';
 import Dicas from '../pages/Dicas';
@@ -8,10 +9,30 @@ const Tab = createBottomTabNavigator();
 
 export default function HomeRoutes() {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={Home} options={{ headerShown: false }} />
-            <Tab.Screen name="Dicas" component={Dicas} />
-            <Tab.Screen name="Conta" component={Conta} />
+        <Tab.Navigator inactiveColor={'black'} activeColor={'#bde4dd'} >
+            <Tab.Screen name="Home" component={Home} options={{
+                headerShown: false,
+                tabBarLabel: 'Home',
+                tabBarIcon: ({ color, size }) => (
+                    <MaterialCommunityIcons name="home" color={color} size={size} />
+                ),
+            }} />
+            <Tab.Screen name="Dicas" component={Dicas} options={{
+                headerStyle: { backgroundColor: '#bde4dd' },
+                tabBarLabel: 'Dicas',
+                tabBarIcon: ({ color, size }) => (
+                    <MaterialCommunityIcons name="bell" color={color} size={size} />
+                ),
+            }} />
+            <Tab.Screen name="Conta" component={Conta}
+                options={{
+                    headerStyle: { backgroundColor: '#bde4dd' },
+                    tabBarIcon: 'Conta',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="account" color={color} backgroundColor={'#bde4dd'} size={size} />
+                    ),
+                }}
+            />
         </Tab.Navigator>
     );
 }
