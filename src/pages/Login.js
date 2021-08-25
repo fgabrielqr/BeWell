a900b57e20d29e0a36d99413816e826c1d49f15e
leocassiosilva <leocassiosilva1234@gmail.com>
2021-08-24 23:48:32 -0300

    get info user login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,32 +1,73 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect} from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity, Alert, Keyboard } from 'react-native';
 import { styles } from '../styles/login';
 import { Input } from '../components/Input';
 import api from '../service/api';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 export default function Login({ navigation }) {
    
+    const keyAsyncStorage = "@bewell:login";
+
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
-    
+
+
+    async function autenticationUser(){
+        try {
+          const retorno = await AsyncStorage.getItem(keyAsyncStorage); 
+          const parseJson = JSON.parse(retorno); 
+  
+          token = (parseJson || '');  
+  
+          const response = await api.get('/meus-dados/',{
+            headers:{
+                'authorization': 'Bearer ' + token,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+          });
+          console.log(response.data);
+          navigation.navigate('Home');
+  
+        } catch(error){
+          console.log("Error Login"); 
+        }
+      }
+
+
+    //function fazer requisição a api 
     async function handleLogin(){
         var params = new URLSearchParams();
         params.append('email', email);
         params.append('password', senha); 
 
-        console.log(email);
-
       try {
             const responseToken = await api.post('api/token/', params);
             const response = responseToken.data;
-            console.log(response);
+            const token = response.access;
+            console.log(token);
+            await AsyncStorage.setItem(keyAsyncStorage, JSON.stringify(token));
+            //console.log(token);
             setEmail(''); 
             setSenha(''); 
+            Keyboard.dismiss(); 
+
+            autenticationUser();
       }catch(error){
-          console.log(error);
-        Alert.alert('Error');
+            console.log(error);
+            Alert.alert('Error');
       }
     }
+
+    async function clear() {
+        await AsyncStorage.clear();
+      }
+  
+    useEffect( ()=> {
+       autenticationUser();
+    }, []);
 
     return (
         <View>
