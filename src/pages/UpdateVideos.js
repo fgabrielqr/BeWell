7a32b155f48f95dc34cb03387189209c0b8a025f
leocassiosilva <leocassiosilva1234@gmail.com>
2021-09-25 12:34:12 -0300

    corrigindo a validação

--- src/pages/UpdateVideos.js
+++ src/pages/UpdateVideos.js
@@ -1,30 +1,28 @@
-import React, { useState, useEffect, useContext} from 'react';
-import { Text, View, TouchableOpacity, Alert} from 'react-native';
-import { styles } from '../styles/cadastro';
-import { Input } from '../components/Input';
-import api from '../service/api';
+import React, { useState, useEffect, useContext, useRef} from 'react';
 import { useAuth } from '../contexts/Auth';
-import { InputForm } from '../components/InputForm';
+import {Formik} from 'formik';
+import {Text, TextInput, View, TouchableOpacity} from 'react-native';
+import * as Yup from 'yup';
+import { styles } from '../styles/cadastro';
+import api from '../service/api';
 
-
-// import form
-import { useForm, Controller } from "react-hook-form";
-import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
-
-// YUP
-const schema = yup.object().shape({
-    nome: yup.string().required("Informe o nome"),
-    url: yup.string().required("Informe a URL"),
-    descricao: yup.string().required("Informe o Descrição"),
-
-});
 
 export default function UpdateVideos({route, navigation }) {
 
-    const [video, setVideo] = useState(route.params ? route.params : {})
+    const [video, setVideo] = useState(route.params ? route.params : {});
     const {user, logout, userLoading} = useAuth();
     const[isLoading,setIsLoading] = useState(false);
+
+    const nome = useRef(null);
+    const url = useRef(null);
+    const descricao = useRef(null);
+
+    const FormSchema = Yup.object().shape({
+        nome: Yup.string().required('Campo obrigatório'),
+        url: Yup.string().required('Campo obrigatório'),
+        descricao: Yup.string().required('Campo obrigatório'),
+    });
+
 
     if(isLoading){
         return(
@@ -34,20 +32,12 @@ export default function UpdateVideos({route, navigation }) {
         )
     } 
 
-
-    // constante retornadas pelo react-hook-form
-    const { control, handleSubmit, formState: { errors } } = useForm({
-        resolver: yupResolver(schema)
-    });
-
-
     //navegation para ir para tela do Listagem de videos
     function navigationToListVideos() {
         navigation.navigate('ListVideos');
     }
 
-
-    //function fazer requisição a api 
+     //function fazer requisição a api 
     async function UpdateVideos(data){
         var body = new FormData();
         body.append('nome', data.nome);
@@ -72,38 +62,75 @@ export default function UpdateVideos({route, navigation }) {
 
     }
 
-
+    
     return (
-        <View>
-            <View style={styles.form}>
-              
-            <Text style={styles.texto}>Nome</Text>
-                <InputForm name='nome' control={control} 
-                    placeholder="Nome"
-                    defaultValue={video.nome}
-                    error={ errors.nome && errors.nome.message } 
-                />
-                <Text style={styles.texto}>URL</Text>
-                <InputForm name='url' control={control} 
-                    placeholder="URL"
-                    defaultValue={video.url}
-                    error={ errors.url && errors.url.message } 
-                />
-                <Text style={styles.texto}>Descricão</Text>
-                <InputForm name='descricao' control={control} 
-                    placeholder="Descricão"
-                    defaultValue={video.descricao}
-                    error={ errors.descricao && errors.descricao.message } 
-                />
-           </View>
+        <Formik
+        initialValues={{
+            nome: video.nome,
+            url: video.url,
+            descricao: video.descricao,
+        }}
+        onSubmit={values => {
+            UpdateVideos(values);
+        }}
+        validationSchema={FormSchema}>
+        {({
+            values,
+            handleChange,
+            handleSubmit,
+            errors,
+            touched,
+            setFieldTouched,
+        }) => (
+            <View >
+                <View style={styles.form}>
 
-           <View>
-                <TouchableOpacity style={styles.btn_entrar} onPress={handleSubmit (UpdateVideos)}>
-                    <Text style={styles.textBtn}>
-                        Editar
-                    </Text>
-                </TouchableOpacity>
+                    <Text style={styles.texto}>Nome</Text>
+                    <View style={styles.containerInput}>
+                        <TextInput
+                            style={styles.inputs} 
+                            ref={nome}
+                            value={values.nome}
+                            onChangeText={handleChange('nome')}
+                            onBlur={() => setFieldTouched('nome', true)}
+                        />
+                        {errors.nome && touched.nome && <Text>{errors.nome}</Text>}
+                    </View>
+
+                    <Text style={styles.texto}>URL</Text>
+                    <View style={styles.containerInput}>
+                        <TextInput
+                            style={styles.inputs} 
+                            ref={url}
+                            value={values.url}
+                            onChangeText={handleChange('url')}
+                            onBlur={() => setFieldTouched('url', true)}
+                        />
+                        {errors.url && touched.url && <Text>{errors.url}</Text>}
+                    </View>
+
+                    <Text style={styles.texto}>Descrição</Text>
+                    <View style={styles.containerInput}>
+                        <TextInput
+                            style={styles.inputs} 
+                            ref={descricao}
+                            value={values.descricao}
+                            onChangeText={handleChange('descricao')}
+                            onBlur={() => setFieldTouched('descricao', true)}
+                        />
+                        {errors.descricao && touched.descricao && <Text>{errors.descricao}</Text>}
+                    </View>
+
+                </View>
+                <View>
+                    <TouchableOpacity style={styles.btn_entrar} onPress={handleSubmit} >
+                        <Text style={styles.textBtn}>
+                            Editar
+                        </Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-        </View>
-    )
+        )}
+        </Formik>
+    );
 };
